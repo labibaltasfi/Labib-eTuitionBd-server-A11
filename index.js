7192f3dbc7488ed51f219a563b584f9d293ec84b
labibaltasfi <labibaltasfi5@gmail.com>
2025-12-17 12:00:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@labibaltasfi.wgwi0xd.mongodb.net/?appName=LabibAlTasfi`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('eTutionbd server is running')
+})
+
+
+async function run() {
+  try {
+    await client.connect();
+     const db = client.db('eTutionbd_db');
+    const usersCollection = db.collection('users');
+
+
+  
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`eTutionbd server is running on port: ${port}`)
+})
