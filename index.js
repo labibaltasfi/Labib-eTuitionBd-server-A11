a44a493c53d20d77601c320d6edf4d997bf14447
labibaltasfi <labibaltasfi5@gmail.com>
2025-12-18 02:16:08 +0600

    add users

--- index.js
+++ index.js
@@ -9,7 +9,27 @@ const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
+
+const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    try {
+        const idToken = token.split(' ')[1];
+        const decoded = await admin.auth().verifyIdToken(idToken);
+        console.log('decoded in the token', decoded);
+        req.decoded_email = decoded.email;
+        next();
+    }
+    catch (err) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@labibaltasfi.wgwi0xd.mongodb.net/?appName=LabibAlTasfi`;
 
@@ -33,6 +53,37 @@ async function run() {
     await client.connect();
      const db = client.db('eTutionbd_db');
     const usersCollection = db.collection('users');
+    
+
+
+    // middle student before allowing student activity
+
+
+    // USERS APIs
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        res.send({ message: 'user already exits. do not need to insert again' })
+      }
+      else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    })
+
+    app.get('/users',verifyFBToken, async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+   
+
 
 
   
